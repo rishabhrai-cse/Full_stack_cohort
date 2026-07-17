cbb673f2f29e50fdbd3b3adae634b187a3056d91
Rishabh Rai <rai735779@gmail.com>
2026-07-17 22:36:17 +0530

    conversion_operations

--- JavaScript/01_basics/datatypes.js
+++ JavaScript/01_basics/datatypes.js
@@ -4,3 +4,22 @@
 //  alert(3+3) // Gives error (we are using node js not browser)
 
 // code readability should be high
+
+let name="rishabh" // String datatype
+let age=19 // num datatype
+let isLoggedIn=true  // Boolean
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
