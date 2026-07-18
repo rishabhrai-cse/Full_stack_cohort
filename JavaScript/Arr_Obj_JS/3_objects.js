ae509158cc390aa3a397b509a9444308ed507160
Rishabh Rai <rai735779@gmail.com>
2026-07-18 13:37:35 +0530

    objects

--- JavaScript/Arr_Obj_JS/3_objects.js
+++ JavaScript/Arr_Obj_JS/3_objects.js
@@ -1,37 +1,37 @@
-// singleton
-// Object.create
+// singleton // apne tarah ka ek hi objects
+// Object.create // constructor  method
 
-// object literals
+// object literals // objects ko declare karne ka tareeka.
 
-const mySym = Symbol("key1")
+const mySym = Symbol("key1") // declaring symbol
 
 
 const JsUser = {
     name: "Hitesh",
-    "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    "full name": "Rishabh Rai", // isko dot se access nhi kar paayenge isliye square bracket use hoga.
+    [mySym]: "mykey1", // symbol is used as key
     age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    location: "Haridwar",
+    email: "Rishabh@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
 
 // console.log(JsUser.email)
-// console.log(JsUser["email"])
+// console.log(JsUser["email"]) // for accesing
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// console.log(JsUser[mySym])  // for symbol  (interview)
 
-JsUser.email = "hitesh@chatgpt.com"
+JsUser.email = "rishabh@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "rishabh@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    console.log(`Hello JS user, ${this.name}`); // string interpolation -->> same object ko reference krna hai to use kro this keyword
 }
 
 console.log(JsUser.greeting());
