3779f6b79a2eb4786b0b0f9a24e4e7b8218ec133
Rishabh Rai <rai735779@gmail.com>
2026-07-23 00:12:22 +0530

    async-function

---
+++ 100x_Devs/Week_1/readfile_async.js
@@ -0,0 +1,36 @@
+//Let’s look at the code to read from a file asynchronously.
+// Here, we pass in a function as an argument. 
+// This function is called a callback 
+// Since the function gets called back when the file is read .
+
+const fs = require("fs");
+fs.readFile("a.txt", "utf-8", function (err, contents) {
+  console.log(contents);
+});
+
+
+// import the fs module
+const fs = require("fs");
+
+// read the file a.txt synchronously and print the content
+// let contents = fs.readFileSync("a.txt", "utf8"); // synchronous
+// console.log(contents);
+
+// function to read the file asynchronously
+function print(err, data) {
+    console.log(data);
+}
+
+// read the file a.txt asynchronously and print the content
+// file path, encoding, callback function
+fs.readFile("a.txt", "utf8", print); // asynchronous
+
+// read the file b.txt asynchronously and print the content
+fs.readFile("b.txt", "utf8", print); // asynchronous
+
+console.log("Done!");
+
+
+// fs.readFile("a.txt", "utf-8", function (err, contents) {
+//     console.log(contents);
+// });
