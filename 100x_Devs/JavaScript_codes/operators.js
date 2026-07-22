81e036700b4efe0353c1efc311ea76e9c9fe0603
Rishabh Rai <rai735779@gmail.com>
2026-07-22 11:08:34 +0530

    operators

--- 100x_Devs/JavaScript_codes/operators.js
+++ 100x_Devs/JavaScript_codes/operators.js
@@ -1,3 +1,9 @@
 let sum = 10 + 5;          // Arithmetic operator
 let isEqual = (10 === 10); // Comparison operator
 let isTrue = (true && false); // Logical operator
+
+
+let age=19;
+let canVote=(age>18); // Either true or False
+console.log(canVote)
+console.log(isTrue)
