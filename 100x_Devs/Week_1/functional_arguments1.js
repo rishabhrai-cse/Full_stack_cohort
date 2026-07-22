33f827af8d5dd914be95bc7192d332d6b2f6a33c
Rishabh Rai <rai735779@gmail.com>
2026-07-22 23:33:44 +0530

    Functional-arguments

---
+++ 100x_Devs/Week_1/functional_arguments1.js
@@ -0,0 +1,22 @@
+//Calling the respective function
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function doOperation(a, b, op) {
+  return op(a, b)
+}
+
+console.log(sum(1, 2))
---
+++ 100x_Devs/Week_1/functional_arguments2.js
@@ -0,0 +1,23 @@
+//Passing in what needs to be done as an argument.
+//Passing a function to another function as an arguments.
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function doOperation(a, b, op) {
+  return op(a, b)
+}
+
+console.log(doOperation(1, 2, sum))
