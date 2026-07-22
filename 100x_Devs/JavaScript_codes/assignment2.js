1c6e12c3c55c1b104f48e8dd0ddea9913a663270
Rishabh Rai <rai735779@gmail.com>
2026-07-22 11:38:54 +0530

    assignment2

---
+++ 100x_Devs/JavaScript_codes/assignment2.js
@@ -0,0 +1,15 @@
+// Write a function sum that finds the sum of two numbers. 
+// Side quest - Try passing in a string instead of a number and see what happens?
+
+function sum(a,b){
+    let ans=a+b;
+    return ans;
+}
+
+let ans1=sum(3,4);
+let ans2=sum(100,400);
+let ans3=sum("Hello ","World");
+console.log(ans1)
+console.log(ans2)
+console.log(ans3)
+
