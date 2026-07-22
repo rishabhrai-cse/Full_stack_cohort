ea366d6734f29d2d485823e1ac1ba6fe894e5b2d
Rishabh Rai <rai735779@gmail.com>
2026-07-22 13:31:10 +0530

    objects-js

---
+++ 100x_Devs/JavaScript_codes/objects.js
@@ -0,0 +1,8 @@
+// An object in JavaScript is a collection of key-value pairs, where each key is a string and each value can be any valid JavaScript data type, including another object.
+
+let user = {
+	name: "Rishabh",
+	age: 19
+}
+
+console.log("Rishabh's  age is " + user.age);
