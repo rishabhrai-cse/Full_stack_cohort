af44f62631113a0124af4d6489e48ea6e68e5a26
Rishabh Rai <rai735779@gmail.com>
2026-07-22 10:49:35 +0530

    variables and assignment in js basics

---
+++ 100x_Devs/JavaScript_codes/assignment1.js
@@ -0,0 +1,9 @@
+// Create a variable for each of the following: your favorite color, your height in centimeters, and whether you like pizza. Use appropriate variable declarations (let, const, or var). Try logging it using console.log
+
+const height=182;
+let fav_color="pink";
+let isPizza=true;
+
+console.log(height)
+console.log(fav_color)
+console.log(isPizza)
---
+++ 100x_Devs/JavaScript_codes/variable.js
@@ -0,0 +1,7 @@
+// var,let,const
+let name = "Rishabh";     // Variable that can be reassigned
+const age = 30;        // Constant variable that cannot be reassigned
+var isStudent = true;  // Older way to declare variables, function-scoped
+console.log(name)
+console.log(age)
+console.log(isStudent)
