458e50b07092d80262f1d3eb5bac978b3e83e1b4
Rishabh Rai <rai735779@gmail.com>
2026-07-22 20:17:44 +0530

    Sum-of-two-num

---
+++ 100x_Devs/Week_1/sumOfTwoNum.js
@@ -0,0 +1,14 @@
+// function sum(a,b){
+//     return a+b;
+// }
+
+// const ans=sum(20,30);
+// console.log(ans);
+
+// if any string is given then we have to pars it into integer
+function sum(a,b){
+    return parseInt(a)+parseInt(b);
+}
+
+const ans=sum("20",30);
+console.log(ans);
