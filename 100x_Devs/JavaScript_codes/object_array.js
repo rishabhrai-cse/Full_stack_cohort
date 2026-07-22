b8bdddcfea8bddb3018f7913f6802564a4a4c665
Rishabh Rai <rai735779@gmail.com>
2026-07-22 14:16:12 +0530

    object-array

---
+++ 100x_Devs/JavaScript_codes/object_array.js
@@ -0,0 +1,13 @@
+const users = [{
+		name: "Rishabh",
+		age: 21
+	}, {
+		name: "raman",
+		age: 22
+	}
+]
+
+const user1 = users[0] 
+console.log(user1);
+const user1Age = users[0].age
+console.log(user1Age);
