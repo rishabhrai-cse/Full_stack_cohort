b5f617a525ec5d88ee5f69ff946c70e1d1e1095c
Rishabh Rai <rai735779@gmail.com>
2026-07-22 22:43:11 +0530

    reading-file

--- 100x_Devs/Week_1/reading-file.js
+++ 100x_Devs/Week_1/reading-file.js
@@ -2,3 +2,5 @@ const fs = require("fs");
 //fs= File system  
 const contents = fs.readFileSync("a.txt", "utf-8"); // bytes,hex (encoding) --> utf-8
 console.log(contents);
+
+// This code is synchronous code.
