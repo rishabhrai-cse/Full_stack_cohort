583a811658170d0a0c9bc69ec6efabeb46d59bfc
Rishabh Rai <rai735779@gmail.com>
2026-07-22 11:55:21 +0530

    assignment

---
+++ 100x_Devs/JavaScript_codes/assignment3.js
@@ -0,0 +1,12 @@
+//Write a function called canVote that returns true or false if the age of a user is > 18
+function canVote(age){
+    if(age>18){
+        return true;
+    }else{
+        return false;
+    } 
+}
+
+let result=canVote(20);
+console.log(result);
+
